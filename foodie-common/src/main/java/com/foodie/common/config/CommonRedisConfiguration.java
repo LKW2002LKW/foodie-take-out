6f259abdba4e9e90b663d4a85035d561f863ff7a
wanderer <2017327832@qq.com>
2026-03-28 22:56:29 +0800

    三端Redis统一重构

---
+++ foodie-common/src/main/java/com/foodie/common/config/CommonRedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.foodie.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class CommonRedisConfiguration {
+
+    @Bean
+    @Primary
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setValueSerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(stringSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
--- foodie-user-server/src/main/java/com/foodie/user/config/RedisConfiguration.java
+++ foodie-user-server/src/main/java/com/foodie/user/config/RedisConfiguration.java
@@ -1,37 +1,8 @@
 package com.foodie.user.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-/**
- * Redis configuration skeleton for user service
- */
 @Configuration
 public class RedisConfiguration {
-
-    /**
-     * Redis配置
-     */
-
-
-        @Bean
-        public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-            RedisTemplate<String, Object> template = new RedisTemplate<>();
-            template.setConnectionFactory(factory);
-
-            // 使用String序列化方式
-            StringRedisSerializer stringSerializer = new StringRedisSerializer();
-            template.setKeySerializer(stringSerializer);
-            template.setValueSerializer(stringSerializer);
-            template.setHashKeySerializer(stringSerializer);
-            template.setHashValueSerializer(stringSerializer);
-
-            template.afterPropertiesSet();
-            return template;
-
-    }
 }
 
